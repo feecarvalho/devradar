bde55cb53acd4ece83a2bde68e66c01a2dd07996
feecarvalho <karvalho_182@hotmail.com>
2020-02-06 03:25:01 -0300

    add: CORS dependence to connect frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -10,8 +11,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-vkedv.mongodb.net/
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 
